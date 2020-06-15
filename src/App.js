e97837609aa26482860379757b0b9f7f0cd8c8fd
korewayume <korewayume@foxmail.com>
2020-06-15 15:01:44 +0800

    Github Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 import SQLEditor from "./components/SQLEditor";
 import "./App.css";
 import schema from "assets/exampleSchema.json"
@@ -14,18 +9,12 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/">
-            <SQLEditor
-              className="SQLEditor"
-              schema={schema.schema}
-              autocompleteEnabled={true}
-              onChange={doNothingHandler}
-              onSelectionChange={doNothingHandler} />
-          </Route>
-        </Switch>
-      </Router>
+      <SQLEditor
+        className="SQLEditor"
+        schema={schema.schema}
+        autocompleteEnabled={true}
+        onChange={doNothingHandler}
+        onSelectionChange={doNothingHandler} />
     </div>
   );
 }
